8865c39965baf6b151db4778c02a4d8ec6c9bf98
Nastya618 <eremeeva-007@mail.ru>
2024-03-05 22:03:06 +0300

    adding theme changesplus saving the current theme to local storage

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -2,28 +2,51 @@ import React from 'react'
 import Select from 'react-select'
 import s from './Header.module.scss'
 import { GlobalSvgSelector } from '../../assets/images/icons/global/GlobalSvgSelector'
-
-const options = [
-	{ value: 'city-1', label: 'Москва' },
-	{ value: 'city-2', label: 'Санкт-Петербург' },
-	{ value: 'city-3', label: 'Волгоград' },
-]
-
-const colourStyles = {
-	control: (styles: any) => ({
-		...styles,
-		backgroundColor: 'rgba(71, 147, 255, 0.2)',
-		width: '194px',
-		height: '37px',
-		border: 'none',
-		borderRadius: '10px',
-		zIndex: 100,
-	}),
-}
+import { Theme } from '../../context/ThemeContext'
+import { useTheme } from '../../hooks/UseTheme'
 
 type Props = {}
 
 export const Header = (props: Props) => {
+	const theme = useTheme()
+	const options = [
+		{ value: 'city-1', label: 'Москва' },
+		{ value: 'city-2', label: 'Санкт-Петербург' },
+		{ value: 'city-3', label: 'Волгоград' },
+	]
+
+	const colourStyles = {
+		control: (styles: any) => ({
+			...styles,
+			backgroundColor:
+				theme.theme === Theme.DARK ? '#4f4f4f' : 'rgba(71, 147, 255, 0.2)',
+			width: '194px',
+			height: '37px',
+			border: 'none',
+			borderRadius: '10px',
+			zIndex: 100,
+			fontWeight: 500,
+		}),
+
+		option: (styles: any) => ({
+			...styles,
+			fontWeight: 500,
+			backgroundColor:
+				theme.theme === Theme.DARK ? '#4f4f4f' : 'rgba(71, 147, 255, 0.2)',
+			borderBottom: '1px solid white',
+			color: theme.theme === Theme.DARK ? '#fff' : '#000',
+		}),
+
+		singleValue: (styles: any) => ({
+			...styles,
+			color: theme.theme === Theme.DARK ? '#fff' : '#000',
+		}),
+	}
+
+	function changeTheme() {
+		theme.changeTheme(theme.theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT)
+	}
+
 	return (
 		<header className={s.header}>
 			<div className={s.wrapper}>
@@ -33,7 +56,7 @@ export const Header = (props: Props) => {
 				<div className={s.title}>React weather</div>
 			</div>
 			<div className={s.wrapper}>
-				<div className={s.change_theme}>
+				<div className={s.change_theme} onClick={changeTheme}>
 					<GlobalSvgSelector id='change-theme' />
 				</div>
 				<Select
